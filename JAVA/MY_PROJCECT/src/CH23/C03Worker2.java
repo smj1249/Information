ff5d02a14ae99730924f88141a2675e2ec2271b6
HTH-0 <xogud1793@naver.com>
2025-03-18 17:05:50 +0900

    no message

---
+++ JAVA/MY_PROJCECT/src/CH23/C03Worker2.java
@@ -0,0 +1,26 @@
+package CH23;
+
+public class C03Worker2  implements Runnable{
+	C03GUI gui;
+	
+	C03Worker2(C03GUI gui){
+		this.gui = gui;
+	}
+	@Override
+	public void run() {
+		
+		while(true) {
+			try {
+				gui.area2.append("WORKER02....\n");	
+				Thread.sleep(500);
+			}
+			catch(InterruptedException  e) {
+				e.printStackTrace();
+				break;
+			}
+		}
+		System.out.println("WORKER02 INTERRUPTED...");
+		
+	}
+
+}
