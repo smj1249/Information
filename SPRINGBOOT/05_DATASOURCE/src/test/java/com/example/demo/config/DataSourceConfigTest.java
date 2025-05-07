24e17f1a6211315e16f3a5521a1bf9ceb8f8ce3e
smj1249 <smj1249@gmail.com>
2025-05-07 14:13:42 +0900

    250507

---
+++ SPRINGBOOT/05_DATASOURCE/src/test/java/com/example/demo/config/DataSourceConfigTest.java
@@ -0,0 +1,38 @@
+package com.example.demo.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+@SpringBootTest
+class DataSourceConfigTest {
+    @Autowired
+    private DataSource dataSource2;
+
+    @Autowired
+    private DataSource dataSource3;
+
+    @Test
+    public void t1() throws Exception{
+        Connection conn = dataSource2.getConnection();
+        PreparedStatement pstmt = conn.prepareStatement("insert into tbl_memo values(?,?,?,now())");
+        pstmt.setInt(1,111);
+        pstmt.setString(2,"ababababab");
+        pstmt.setString(3,"springboot@test.com");
+        pstmt.executeUpdate();
+    }
+
+    @Test
+    public void t2() throws Exception{
+        Connection conn = dataSource3.getConnection();
+        PreparedStatement pstmt = conn.prepareStatement("insert into tbl_memo values(?,?,?,now())");
+        pstmt.setInt(1,333);
+        pstmt.setString(2,"ababababab");
+        pstmt.setString(3,"springboot@test.com");
+        pstmt.executeUpdate();
+    }
+}
